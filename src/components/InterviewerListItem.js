e093cd6dcc327e450a1544a4e88796d45b9bab5c
Faez Catrina <faezcatrina@Faezs-MacBook-Pro.local>
2021-10-28 15:51:29 -0600

    complete application form - actually works finally

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,14 +8,17 @@ import classNames from "classnames";
 
 function InterviewerListItem(props) {
   const interviewerName = props.name;
-  console.log(props);
 
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
   });
 
   return (
-    <li className={interviewerClass} onClick={props.setInterviewer}>
+    <li
+      className={interviewerClass}
+      onClick={props.setInterviewer}
+      selected={props.selected}
+    >
       <img
         className="interviewers__item-image"
         src={props.avatar}
