d393f3788ce8f223cee68ca1e4e9de342e81ab69
Faez Catrina <faezcatrina@Faezs-MacBook-Pro.local>
2021-10-31 19:15:27 -0600

    added mode and mode functionality to the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Form from "components/Appointment/Form";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import { Fragment } from "react";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Fragment>
         <Header time={props.time} />
-        {props.interview ? (
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
           <Show
             student={props.interview.student}
             interviewer={props.interview.interviewer}
           />
-        ) : (
-          <Empty />
         )}
+        {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
       </Fragment>
     </article>
   );
